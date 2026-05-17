63fbd037e762b814327e79ecfa94fa6d0dd418f9
dhimanharsh01 <dhimanharsh809@gmail.com>
2026-05-17 14:49:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version
+
+// alert (3+3) // we are using nodejds, not browser 
+
+console.log(3+3) // this is how we print in nodejs
+
+let name = "John" // string
+let age = 30 // number
+let isMarried = false // boolean
+
+console.log(name)
+console.log(age)
+console.log(isMarried)
+
+// number => 2 to power of 53
+// bigint => for numbers bigger than 2 to power of 53
+// string => "sequence of characters"
+// boolean => true or false
+// null => empty value
+// undefined => value is not assigned
+// symbol => unique identifier
+// object => collection of properties
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
